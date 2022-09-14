ecc067fbf588eb5a4af67a477096ac31f078ccbb
Chiranshu <chiranshugoel@gmail.com>
2022-09-14 15:25:48 +0530

    New Commit

---
+++ pages/chiranshu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Name(props) {
+    return (
+        <div className='text-center container mx-auto px-4'>
+            <h1>My Name is Chiranshu</h1>
+        </div>
+    )
+}
+
+export default Name
